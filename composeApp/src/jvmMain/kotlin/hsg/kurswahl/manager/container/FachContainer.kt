983bdfcc6e85079e8a852edcb776356b75944247
hsgschueler <fadsac12@pm.me>
2026-02-09 11:13:11 +0100

    Variablen in Container Classes Hinzufügen

--- composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/container/FachContainer.kt
+++ composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/container/FachContainer.kt
@@ -1,4 +1,7 @@
 package hsg.kurswahl.manager.container
 
-class FachContainer {
-}
+import hsg.kurswahl.manager.dataClass.Fach
+
+data class FachContainer (
+    var eintraege: MutableList<Fach>,
+)
