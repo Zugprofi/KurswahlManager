e1e66f9debac4974888d102b47253cb32e164ae8
Till Wetzlich <till@wetzlich.me>
2026-02-16 10:59:58 +0100

    Container angepasst

--- composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/container/SchuelerContainer.kt
+++ composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/container/SchuelerContainer.kt
@@ -1,7 +1,8 @@
 package hsg.kurswahl.manager.container
 
 import hsg.kurswahl.manager.dataClass.Schueler
+import java.util.Collections
 
 class SchuelerContainer (
-    var eintraege: MutableMap<String, Schueler>
+    var eintraege: MutableMap<String, Schueler> = Collections.emptyMap()
 )
