9409cc498dba23edebf2e5c3a226b70dbc6449ef
hsgschueler <fadsac12@pm.me>
2026-03-10 19:39:40 +0100

    Hinzufügen von der class Fs3Art und änderung an der Schüler class. (Rausnehmen von den Prios und den data type von boolean zu der klasse fs3art geändert

--- composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/dataClass/Schueler.kt
+++ composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/dataClass/Schueler.kt
@@ -1,22 +1,19 @@
 package hsg.kurswahl.manager.dataClass
 
-import kurswahlmanager.composeapp.generated.resources.Res
-
 class Schueler (
     val id: String,
     val vorname: String,
     val nachname: String,
-    val mail: String,
 
     // FS3
     val fs3Id: Int? = null,
     var fs3: Fach? = null,
-    val fs3Weiterfuehren: Boolean = true,
+    val fs3Art: Fs3Art = Fs3Art.VIER_STUENDIG,
     val asb: Boolean? = null,
 
     // WPU Wahlen
-    val wpuWahl1Ids: MutableList<Int> = mutableListOf(), // 3 Prios
-    val wpuWahl2Ids: MutableList<Int> = mutableListOf(), // 3 Prios
+    val wpuWahl1Ids: MutableList<Int> = mutableListOf(),
+    val wpuWahl2Ids: MutableList<Int> = mutableListOf(),
 
     var wpuWahl1: MutableList<Fach> = mutableListOf(),
     var wpuWahl2: MutableList<Fach> = mutableListOf(),
