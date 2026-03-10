caa085b8761ad1f2cd815127d252bc3c630c9b75
hsgschueler <fadsac12@pm.me>
2026-03-10 20:03:51 +0100

    Erstellung des Algorithmus für die Zuweisung und die notwendigen Funktionen

--- composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/dataClass/Schueler.kt
+++ composeApp/src/jvmMain/kotlin/hsg/kurswahl/manager/dataClass/Schueler.kt
@@ -1,9 +1,9 @@
 package hsg.kurswahl.manager.dataClass
 
-class Schueler (
-    val id: String,
-    val vorname: String,
-    val nachname: String,
+class Schueler(
+    val id: String = "",
+    val vorname: String = "",
+    val nachname: String = "",
 
     // FS3
     val fs3Id: Int? = null,
@@ -11,13 +11,15 @@ class Schueler (
     val fs3Art: Fs3Art = Fs3Art.VIER_STUENDIG,
     val asb: Boolean? = null,
 
-    // WPU Wahlen
+    // WPU Wahlen (IDs der gewählten Fächer, je 3 Stück)
     val wpuWahl1Ids: MutableList<Int> = mutableListOf(),
     val wpuWahl2Ids: MutableList<Int> = mutableListOf(),
 
+    // WPU Wahlen als Fach-Objekte (werden nach dem Laden befüllt)
     var wpuWahl1: MutableList<Fach> = mutableListOf(),
     var wpuWahl2: MutableList<Fach> = mutableListOf(),
 
+    // Ergebnis der Zuweisung (wird vom Algorithmus gesetzt)
     var wpu1: Fach? = null,
     var wpu2: Fach? = null
 )
